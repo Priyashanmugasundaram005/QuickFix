18eef0ac37999310bffc26bb174c321c52750617
Priyashanmugasundaram005 <priyashanmugasundaram2005@gmail.com>
2026-02-23 18:06:39 +0530

    Readme

---
+++ quickfix/api.py
@@ -0,0 +1,45 @@
+import frappe
+from frappe.utils import now_datetime, add_days
+from frappe.query_builder import DocType
+
+
+@frappe.whitelist()
+def get_overdue_jobs():
+    JC = DocType("Job Card")
+    seven_days_ago = add_days(now_datetime(), -7)
+
+    overdue_jobs = (
+        frappe.qb.from_(JC)
+        .select(JC.name,JC.customer_name,JC.assigned_technician,JC.creation)
+        .where(
+            (JC.status.isin(["Pending Diagnosis", "In Repair"])) &
+            (JC.creation < seven_days_ago)
+        )
+        .orderby(JC.creation)  
+        .run(as_dict=True)
+    )
+
+    return overdue_jobs
+
+
+
+
+@frappe.whitelist()
+def transfer_job(from_tech, to_tech):
+    try:
+        frappe.db.sql(
+            """
+            UPDATE `tabJob Card`
+            SET assigned_technician = %s
+            WHERE assigned_technician = %s
+              AND status IN ('Pending Diagnosis', 'In Repair')
+            """,
+            (to_tech, from_tech),
+        )
+        frappe.db.commit()
+        return "Success"
+
+    except Exception:
+        frappe.db.rollback()
+        frappe.log_error(frappe.get_traceback(), "Job Transfer Failed")
+        raise
---
+++ quickfix/config/desktop.py
@@ -0,0 +1,10 @@
+import frappe
+
+def get_data():
+    return [
+        {
+            "module_name": "Service Center",
+            "type": "module",
+            "label": "Service Center"
+        }
+    ]
